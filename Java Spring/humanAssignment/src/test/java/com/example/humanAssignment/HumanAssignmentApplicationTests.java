28a74c33f471921747504f49d2f117cc8006cbc6
khalil <johndoe@example.com>
2021-06-16 15:57:38 +0300

    humanAssignment

---
+++ Java Spring/humanAssignment/src/test/java/com/example/humanAssignment/HumanAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
